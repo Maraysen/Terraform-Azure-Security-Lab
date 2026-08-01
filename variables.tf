6bc7c2f2056297cf3a137753f7158ce7e29a7f75
Mike Kobeissi <mike.kobeissi@outlook.com>
2026-08-01 16:52:57 +1000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "location" {
+  description = "Azure region for the lab resources"
+  type        = string
+  default     = "Australia East"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-terraform-security-lab"
+}
+
+variable "common_tags" {
+  description = "Standard tags applied to supported resources"
+  type        = map(string)
+
+  default = {
+    Environment = "Lab"
+    Project     = "Terraform-Azure-Security"
+    ManagedBy   = "Terraform"
+  }
+}
+
+variable "lab_secret" {
+  description = "Test secret stored in Azure Key Vault"
+  type        = string
+  sensitive   = true
+}
