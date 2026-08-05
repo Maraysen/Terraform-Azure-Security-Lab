5fce5fb47dba7ca458fea84cfe256585a6082864
Mike Kobeissi <310149564+Maraysen@users.noreply.github.com>
2026-08-05 18:37:09 +1000

    Disable shared key access on storage accounts

--- storage.tf
+++ storage.tf
@@ -9,6 +9,7 @@ resource "azurerm_storage_account" "lab" {
   https_traffic_only_enabled      = true
   min_tls_version                 = "TLS1_2"
   allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false
 
   # checkov:skip=CKV2_AZURE_41:SAS expiration policy is explicitly configured; Checkov does not recognise the current AzureRM syntax.
   sas_policy {
@@ -32,6 +33,7 @@ resource "azurerm_storage_account" "backup" {
   min_tls_version                 = "TLS1_2"
   https_traffic_only_enabled      = true
   allow_nested_items_to_be_public = false
+  shared_access_key_enabled       = false
 
   # checkov:skip=CKV2_AZURE_41:SAS expiration policy is explicitly configured; Checkov does not recognise the current AzureRM syntax.
   sas_policy {
