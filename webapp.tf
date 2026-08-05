76669233859403bb14018fbc96aac880e9dfc3c4
Mike Kobeissi <310149564+Maraysen@users.noreply.github.com>
2026-08-05 18:55:43 +1000

    Harden App Service network settings

--- webapp.tf
+++ webapp.tf
@@ -14,6 +14,8 @@ resource "azurerm_linux_web_app" "lab" {
   service_plan_id     = azurerm_service_plan.lab.id
   tags                = var.common_tags
 
+  https_only = true
+
   identity {
     type = "SystemAssigned"
   }
@@ -23,6 +25,8 @@ resource "azurerm_linux_web_app" "lab" {
   }
 
   site_config {
-    always_on = false
+    always_on     = false
+    ftps_state    = "Disabled"
+    http2_enabled = true
   }
 }
